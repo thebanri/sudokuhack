e324c809689e80c2f8d8e8d8ebf3429eba7f7a54
Arslan Aydin <41839715+thebanri@users.noreply.github.com>
2024-09-03 23:09:51 +0300

    Update Sudoku.java

--- Sudoku.java
+++ Sudoku.java
@@ -7,7 +7,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-public class Sudoku { 
+public class Sudoku { ][{{[{[{[{[{[{[{[{[{[{[
     public static void main(String[] args) {
 
         int[][] matrix = {
